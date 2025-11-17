85697fd45af0b0599b5979842b0f4863cd0a131b
Matej Čurčić <matej.curcic@pontistechnology.com>
2025-11-17 10:08:46 +0100

    Added Cart and Cart item models (classes)

---
+++ RetailPlatform.Domain/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace RetailPlatform.Domain.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<CartItem> Items { get; set; } = [];
+    }
+}
---
+++ RetailPlatform.Domain/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace RetailPlatform.Domain.Models
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public Guid CartId { get; set; }
+        public string Sku { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public Cart Cart { get; set; } = null!;
+    }
+}
